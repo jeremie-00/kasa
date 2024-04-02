7b1cc5ff4965a66d60b069b174f01f240d3a9cb6
jeremie <jeremie.herault@orange.fr>
2024-04-02 14:09:09 +0200

    upload header.jsx

---
+++ src/layouts/header/header.jsx
@@ -0,0 +1,15 @@
+import logoHeader from '../../assets/logo_header.png';
+import { NavLink } from 'react-router-dom';
+//import './header.css'
+
+function Header() {
+    return <header className='header'>
+      <img className='logo' src={logoHeader} alt='logo kasa' />
+      <nav className='nav'>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/about">A propos</NavLink>
+      </nav>
+    </header>
+  }
+
+export default Header;
--- src/roots/rootOutlet.jsx
+++ src/roots/rootOutlet.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
+import Header from "../layouts/header/header";
 
 export default function RootOutlet() {
     return <>
       <div className='main-container'>
-
+        <Header />
         <section className='container'>
           <Outlet />
         </section>
-        
+
       </div>
     </>
   }
