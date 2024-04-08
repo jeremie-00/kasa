661c95d2641bdac66173b368764518353d1fbb28
jeremie <jeremie.herault@orange.fr>
2024-04-08 14:10:52 +0200

    animation opacity slider

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -6,8 +6,6 @@ import arrowRight from "@assets/arrow_right.png"
 export default function SlideShow({ pictures }) {
     const [activeIndex, setActiveIndex] = useState(0)
 
-    
-
     const handlePrev = () => {
         // prevIndex = valeur précedente fonction "callback"
         // si prevIndex === 0 alors on affiche la derniere picture sinon -1
@@ -16,14 +14,22 @@ export default function SlideShow({ pictures }) {
 
     const handleNext = () => {
         // si prevIndex === la derniere picture alors on renvoi 0 sinon +1
-        //console.log(pictures.length - 1)
         setActiveIndex((prevIndex) => (prevIndex === pictures.length - 1 ? 0 : prevIndex + 1))
     }
 
     return <div className='slider flexRow' >
-        <img className="slide-img" src={pictures[activeIndex]} alt="image appartement" />
-        <img className={pictures.length <= 1 ? 'hidden' : 'arrow arrow_left'} src={arrowLeft} alt="arrow left" onClick={handlePrev}/>
-        <img className={pictures.length <= 1 ? 'hidden' : 'arrow arrow_right'} src={arrowRight} alt="arrow right" onClick={handleNext}/>
+
+            {pictures.map((picture, index) => (
+                <img
+                    key={index}
+                    className= {`slide-img ${index === activeIndex ? `active` : ``}`}
+                    src={picture}
+                    alt={`image ${index}`}
+                />
+            ))}
+            
+        <img className={pictures.length <= 1 ? 'hidden' : 'arrow arrow_left'} src={arrowLeft} alt="arrow left" onClick={handlePrev} />
+        <img className={pictures.length <= 1 ? 'hidden' : 'arrow arrow_right'} src={arrowRight} alt="arrow right" onClick={handleNext} />
         <span className={pictures.length <= 1 ? 'hidden' : 'numerotation'} >{activeIndex + 1}/{pictures.length}</span>
     </ div>
 }
