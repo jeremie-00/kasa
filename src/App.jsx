095656b9ad6d904d13aac252f979c8df22f85c4a
jeremie <jeremie.herault95@orange.fr>
2024-04-30 08:27:40 +0200

    sup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import './App.scss'
 
 import Router from "@roots/router";
-import Spinner from "@components/spinner";
+
 export default function App() {
   const router = Router()
   return <RouterProvider router={router}/>
