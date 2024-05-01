d5e636aacadd733e36fe6bcd4e1083ed5c7b3a04
jeremie <jeremie.herault95@orange.fr>
2024-05-01 10:00:34 +0200

    modif

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jeremie-00.github.io/kasa/',
+  base: '/kasa/',
   build: {
     outDir: 'dist',
   },
