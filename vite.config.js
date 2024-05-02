19c687149c67db7406f86d38193394af1b9edf51
jeremie <134471174+jeremie-00@users.noreply.github.com>
2024-05-02 13:18:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://jeremie-00.github.io/src/main.jsx',
+  base: '/kasa',
   resolve: {
     alias: [
       {
