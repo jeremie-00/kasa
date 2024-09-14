2d499d171a6dd6fc6c2721ade17259a3b6530d4e
jeremie <jeremie.herault95@orange.fr>
2024-09-14 08:35:44 +0200

    test

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,14 +1,15 @@
-import logoHeader from '../assets/logo_header.png';
-import { NavLink } from 'react-router-dom';
+import logoHeader from "../assets/logo_header.png";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
-
   return (
     <div className="wrapper-header">
-      <header className='header'>
-        <img className='logo' src={logoHeader} alt='logo kasa' />
-        <nav className='nav'>
-          <NavLink to="/" end>Accueil</NavLink>
+      <header className="header">
+        <img className="logo" src={logoHeader} alt="logo kasa" />
+        <nav className="nav">
+          <NavLink to="/" end>
+            Accueil
+          </NavLink>
           <NavLink to="/about">A propos</NavLink>
         </nav>
       </header>
--- src/roots/router.jsx
+++ src/roots/router.jsx
@@ -1,60 +1,58 @@
 import React from "react";
-import { createBrowserRouter, defer, redirectDocument  } from 'react-router-dom';
+import { createBrowserRouter, defer } from "react-router-dom";
 
-import RootOutlet from "./rootOutlet";
-import PageError from "../pages/error"
-import Home from "../pages/home";
 import About from "../pages/about";
+import PageError from "../pages/error";
+import Home from "../pages/home";
 import Location from "../pages/location";
-
+import RootOutlet from "./rootOutlet";
 
 export default function Router() {
-
-    const router = createBrowserRouter ([
+  const router = createBrowserRouter([
+    {
+      // basename:'/kasa/',
+      path: "/",
+      element: <RootOutlet />,
+      //affiche la page erreur si une erreur est capturée (url ou composent)
+      errorElement: <PageError />,
+      children: [
         {
-            // basename:'/kasa/',
-            path: '/',
-            element: <RootOutlet />,
-            //affiche la page erreur si une erreur est capturée (url ou composent)
-            errorElement: <PageError />,
-            children: [
-                {
-                    path: '/',
-                    element: <Home />,
-                    loader: async () =>  {
-                        const data = await fetch('/logements.json').then(response => response.json())
-                        return defer({ data })
-                    },
-                    
-                },
-                {
-                    path: 'about',
-                    element: <About />,
-                    loader: async () => {
-                        const data = await fetch('/texte_collaps.json').then(response => response.json())
-                        return defer({ data })
-                    },
-
-                },
-                {
-                    path: 'location/:id',
-                    element: <Location />,
-                    loader: async ({ params }) => {
-                        const data = await fetch('/logements.json').then(response => response.json())
-                        const locationData = data.find((d) => d.id === params.id)
-                        if (locationData) {
-                            return defer({ locationData })
-                        } else {
-                            throw new Error("id logement non trouver")
-                        }
-
-                    },
-
-                },
-
-            ]
-        }
-    ])
-    return router
+          path: "/",
+          element: <Home />,
+          loader: async () => {
+            const data = await fetch("/logements.json").then((response) =>
+              response.json()
+            );
+            return defer({ data });
+          },
+        },
+        {
+          path: "about",
+          element: <About />,
+          loader: async () => {
+            const data = await fetch("/texte_collaps.json").then((response) =>
+              response.json()
+            );
+            return defer({ data });
+          },
+        },
+        {
+          path: "location/:id",
+          element: <Location />,
+          loader: async ({ params }) => {
+            const data = await fetch("/logements.json").then((response) =>
+              response.json()
+            );
+            const locationData = data.find((d) => d.id === params.id);
+            if (locationData) {
+              return defer({ locationData });
+            } else {
+              throw new Error("id logement non trouver");
+            }
+          },
+        },
+      ],
+    },
+  ]);
+  return router;
 }
-
